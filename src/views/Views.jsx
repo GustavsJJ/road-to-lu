995ca1ef7ebf93cad18b7585ee3f48d72143b186
Gustavs Jānis Jākobsons <gustavs.jakobsons@gmail.com>
2021-06-09 15:59:52 +0300

    feat: views page

--- src/views/Views.jsx
+++ src/views/Views.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { withNamespaces } from "react-i18next";
 import { NextChapter } from "../components/NextChapter";
 import Chapter from "../components/Chapter/Chapter";
+import logs from "../images/logs.jpg";
+import udenskritums from "../images/udenskritums.jpg";
 
 function Views({ t }) {
   return (
     <div class="views">
-      <Chapter title="VIEWS" description="DESC" />
+      <Chapter title="VIEWS" description="VIEWS_DESC" />
       <div className="container">
-        <h5>{t("Welcome to React")}</h5>
+        <img className="hero-image" src={logs} alt="logs" />
+        <p>{t("VIEWS1")}</p>
+        <img className="hero-image" src={udenskritums} alt="udenskritums" />
+        <p>{t("VIEWS2")}</p>
       </div>
       <NextChapter to="/music" />
     </div>
